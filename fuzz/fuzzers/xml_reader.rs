e322e26b7e8c3dbfcba5c6142ca7559b63fc1652
Edward Barnard <eabarnard@gmail.com>
2017-03-06 21:07:19 +0000

    Fuzz the binary and xml parsers separately.

--- fuzz/fuzzers/xml_reader.rs
+++ fuzz/fuzzers/xml_reader.rs
@@ -4,7 +4,7 @@ extern crate plist;
 
 use std::io::Cursor;
 use plist::Plist;
-use plist::binary::EventReader;
+use plist::xml::EventReader;
 
 #[export_name="rust_fuzzer_test_input"]
 pub extern fn go(data: &[u8]) {
