1a60c49909c4dfffa112a7b9e63dbf87224c2e16
Edward Barnard <eabarnard@gmail.com>
2017-05-05 09:54:35 +0100

    Update cargo fuzz.

--- fuzz/fuzzers/xml_reader.rs
+++ fuzz/fuzzers/xml_reader.rs
@@ -1,14 +1,13 @@
 #![no_main]
-extern crate libfuzzer_sys;
+#[macro_use] extern crate libfuzzer_sys;
 extern crate plist;
 
 use std::io::Cursor;
 use plist::Plist;
 use plist::xml::EventReader;
 
-#[export_name="rust_fuzzer_test_input"]
-pub extern fn go(data: &[u8]) {
+fuzz_target!(|data: &[u8]| {
     let cursor = Cursor::new(data);
     let reader = EventReader::new(cursor);
     let _ = Plist::from_events(reader);
-}
+});
