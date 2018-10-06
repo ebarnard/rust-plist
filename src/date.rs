205c980cf0c75c6b8267491ade1e45cb2e91a61d
Edward Barnard <eabarnard@gmail.com>
2018-10-06 20:21:53 +0100

    Manually implement Hash for Date to keep minimum supported Rust version at 1.18.0.

--- src/date.rs
+++ src/date.rs
@@ -1,10 +1,11 @@
 use humantime;
 use std::fmt;
+use std::hash::{Hash, Hasher};
 use std::result::Result as StdResult;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 /// A UTC timestamp. Used for serialization to and from the plist date type.
-#[derive(Clone, Copy, Hash, PartialEq)]
+#[derive(Clone, Copy, PartialEq)]
 pub struct Date {
     inner: SystemTime,
 }
@@ -51,6 +52,23 @@ impl fmt::Debug for Date {
     fn fmt(&self, f: &mut fmt::Formatter) -> StdResult<(), fmt::Error> {
         let rfc3339 = humantime::format_rfc3339(self.inner);
         <humantime::Rfc3339Timestamp as fmt::Display>::fmt(&rfc3339, f)
+    }
+}
+
+// TODO: Remove manual impl once minimum Rust version reaches 1.24.0.
+impl Hash for Date {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let elapsed = match self.inner.duration_since(UNIX_EPOCH) {
+            Ok(elapsed) => {
+                false.hash(state);
+                elapsed
+            }
+            Err(err) => {
+                true.hash(state);
+                err.duration()
+            }
+        };
+        elapsed.hash(state)
     }
 }
 
