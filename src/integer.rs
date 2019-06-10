5ee33d75dc16235d1c2a0a36a0270805cfbbe2a4
svartalf <self@svartalf.info>
2019-06-10 19:32:56 +0300

    Support parsing NetBSD proplists which encode unsigned integers as hexadecimal strings.

--- src/integer.rs
+++ src/integer.rs
@@ -42,12 +42,22 @@ impl FromStr for Integer {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // Match Apple's implementation in CFPropertyList.h - always try to parse as an i64 first.
-        // TODO: Use IntErrorKind once stable and retry parsing on overflow only.
-        Ok(match s.parse::<i64>() {
-            Ok(v) => v.into(),
-            Err(_) => s.parse::<u64>()?.into(),
-        })
+        if s.starts_with("0x") {
+            // NetBSD dialect adds the `0x` numeric objects,
+            // which are always unsigned.
+            // See the `PROP_NUMBER(3)` man page
+            let s = s.trim_left_matches("0x");
+
+            u64::from_str_radix(s, 16).map(Into::into)
+        } else {
+            // Match Apple's implementation in CFPropertyList.h - always try to parse as an i64 first.
+            // TODO: Use IntErrorKind once stable and retry parsing on overflow only.
+            Ok(match s.parse::<i64>() {
+                Ok(v) => v.into(),
+                Err(_) => s.parse::<u64>()?.into(),
+            })
+        }
+
     }
 }
 
