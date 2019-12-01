0ee381706f49b827b18174ae20fd2f5bb06bbe84
Francois Stephany <francois@tamere.eu>
2019-12-01 18:20:07 +0100

    Bubble up io::Error when reading a char from the Reader
    Use read_exact() on the Reader instead of read(). 
    The setup code in `new()` has been replaced by a check in `advance()`.

--- src/error.rs
+++ src/error.rs
@@ -37,6 +37,7 @@ pub(crate) enum ErrorKind {
     UnclosedString,
     IncompleteComment,
     InvalidUtf8AsciiStream,
+    IoReadError,
 
     // Xml format-specific errors
     UnclosedXmlElement,
