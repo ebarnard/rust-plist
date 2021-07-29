913670fada7a8a3c312fe4283be7296a33da5664
Colin Rofls <colin@cmyr.net>
2021-07-29 16:07:40 -0400

    Add ser::to_writer_xml_with_options
    This was an oversight, and should've been added along with the equivalent
    method on Value.

--- src/lib.rs
+++ src/lib.rs
@@ -112,7 +112,9 @@ pub use self::{de::Deserializer, ser::Serializer};
 #[cfg(feature = "serde")]
 pub use self::{
     de::{from_bytes, from_file, from_reader, from_reader_xml},
-    ser::{to_file_binary, to_file_xml, to_writer_binary, to_writer_xml},
+    ser::{
+        to_file_binary, to_file_xml, to_writer_binary, to_writer_xml, to_writer_xml_with_options,
+    },
 };
 
 #[cfg(all(test, feature = "serde"))]
