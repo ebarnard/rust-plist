ebarnard/rust-plist

commit ddff8e6ab781e1717b000034cee8a43b505534fc

Silence warnings that only appear in some build configurations

src/stream/xml_writer.rs
@@ -35,6 +35,7 @@ pub struct XmlWriter<W: Write> {
 }
 
 impl<W: Write> XmlWriter<W> {
+    #[cfg(fearure = "enable_unstable_features_that_may_break_with_minor_version_bumps")]
     pub fn new(writer: W) -> XmlWriter<W> {
         let opts = XmlWriteOptions::default();
         XmlWriter::new_with_options(writer, &opts)
@@ -60,6 +61,11 @@ impl<W: Write> XmlWriter<W> {
             expecting_key: false,
             empty_namespace: Namespace::empty(),
         }
+    }
+
+    #[cfg(feature="enable_unstable_features_that_may_break_with_minor_version_bumps")]
+    pub fn into_inner(self) -> W {
+        self.xml_writer.into_inner()
     }
 
     fn write_element_and_value(&mut self, name: &str, value: &str) -> Result<(), Error> {
@@ -94,10 +100,6 @@ impl<W: Write> XmlWriter<W> {
             .write(XmlEvent::Characters(value))
             .map_err(from_xml_error)?;
         Ok(())
-    }
-
-    pub fn into_inner(self) -> W {
-        self.xml_writer.into_inner()
     }
 
     fn write_event<F: FnOnce(&mut Self) -> Result<(), Error>>(

src/error.rs
@@ -19,7 +19,9 @@ pub(crate) enum ErrorKind {
     UnexpectedEof,
     UnexpectedEndOfEventStream,
     UnexpectedEventType {
+        #[allow(dead_code)]
         expected: EventKind,
+        #[allow(dead_code)]
         found: EventKind,
     },
 
@@ -55,6 +57,7 @@ pub(crate) enum ErrorKind {
     UnknownObjectType(u8),
 
     Io(io::Error),
+    #[cfg(feature = "serde")]
     Serde(String),
 }
 

commit 5fdedbe9a1dac3cc6f48355d4b51092965d3efeb

fix typo in feature gate, introduced in ddff8e6

src/stream/xml_writer.rs
@@ -36,7 +36,7 @@ enum PendingCollection {
 }
 
 impl<W: Write> XmlWriter<W> {
-    #[cfg(fearure = "enable_unstable_features_that_may_break_with_minor_version_bumps")]
+    #[cfg(feature = "enable_unstable_features_that_may_break_with_minor_version_bumps")]
     pub fn new(writer: W) -> XmlWriter<W> {
         let opts = XmlWriteOptions::default();
         XmlWriter::new_with_options(writer, &opts)