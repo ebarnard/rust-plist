be53fbc88205658ba450e3cdb99b561a011aaccb
Simon Rupf <simon@rupf.net>
2025-07-06 11:42:29 +0200

    Address clippy lints
    This addresses almost all regular clippy lints and some of the pedantic ones.
    
    I left this one alone, as I personally don't find it more readable nor does it look very efficient:
    ```
    error: manual `!RangeInclusive::contains` implementation
       --> src/stream/ascii_reader.rs:361:8
        |
    361 |     if index < 128 || index > 0xff {
        |        ^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: use: `!(128..=0xff).contains(&index)`
        |
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#manual_range_contains
    ```

--- src/value.rs
+++ src/value.rs
@@ -683,7 +683,7 @@ impl Builder {
                 self.stack.push(StackItem::Array(array));
             }
             (Some(StackItem::Dict(dict)), Value::String(key)) => {
-                self.stack.push(StackItem::DictAndKey(dict, key))
+                self.stack.push(StackItem::DictAndKey(dict, key));
             }
             (Some(StackItem::Dict(_)), value) => {
                 return Err(ErrorKind::UnexpectedEventType {
@@ -809,7 +809,7 @@ mod tests {
         assert_eq!(Value::Integer(1.into()).as_unsigned_integer(), Some(1));
         assert_eq!(Value::Integer((-1).into()).as_unsigned_integer(), None);
         assert_eq!(
-            Value::Integer((i64::max_value() as u64 + 1).into()).as_signed_integer(),
+            Value::Integer((i64::MAX as u64 + 1).into()).as_signed_integer(),
             None
         );
         assert_eq!(Value::String("2".to_owned()).as_string(), Some("2"));
